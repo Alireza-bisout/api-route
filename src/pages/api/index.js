85e78483ebe39ce05bc9ca1edc764cf107c2ccf5
alireza_bisout <alirezabisout1145@gmail.com>
2025-03-03 18:38:02 +0330

    ...

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,26 +1,32 @@
 export default function handler(req, res) {
-    res.status(200).json(
-        [
-            {
-                id: 1,
-                todo: "tomorrow is a good day for training task 1"
-            },
-            {
-                id: 2,
-                todo: "tomorrow is a good day for training task 2"
-            },
-            {
-                id: 3,
-                todo: "tomorrow is a good day for training task 3"
-            },
-            {
-                id: 4,
-                todo: "tomorrow is a good day for training task 4"
-            },
-            {
-                id: 5,
-                todo: "tomorrow is a good day for training task 5"
-            },
-        ]
-    )
+
+    if (req.method === "GET") {
+        res.status(200).json(
+            [
+                {
+                    id: 1,
+                    todo: "tomorrow is a good day for training task 1"
+                },
+                {
+                    id: 2,
+                    todo: "tomorrow is a good day for training task 2"
+                },
+                {
+                    id: 3,
+                    todo: "tomorrow is a good day for training task 3"
+                },
+                {
+                    id: 4,
+                    todo: "tomorrow is a good day for training task 4"
+                },
+                {
+                    id: 5,
+                    todo: "tomorrow is a good day for training task 5"
+                },
+            ]
+        )
+    } else if (req.method === "POST") {
+        const todo = req.body
+        console.log(todo)
+    }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,31 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState();
 
   useEffect(() => {
+
     async function fetchData() {
       const res = await fetch('/api');
       const data = await res.json();
+
       setTodos(data);
       console.log(data);
     }
+
     fetchData();
   }, []);
+
+  const clickHandler = async () => {
+    const res = fetch("/api", {
+      method: "POST",
+      body: JSON.stringify({ todo }),
+      headers: { "Content-Type": "application/json" }
+    })
+    const data = await (await res).json()
+    console.log(data)
+  }
+
   return (
     <>
       <ul>
@@ -20,6 +35,10 @@ export default function Home() {
             <li key={todo.id}>{todo.todo}</li>
           ))
         }
+
+        <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
+
+        <button onClick={clickHandler}>create todo</button>
       </ul>
     </>
   );
